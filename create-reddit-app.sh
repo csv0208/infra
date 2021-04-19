a0c5c9c25f8f875a53d7145635b658bff0b91dc7
Sergey Cherevko <csv0208@gmail.com>
2021-04-19 16:55:25 +0300

    lesson 6 D3

--- create-reddit-app.sh
+++ create-reddit-app.sh
@@ -1,26 +1,5 @@
-# infra
+#!/bin/bash
 
-D5:
-Подключение к bastion:
-
-ssh -i ~/.ssh/appuser appuser@34.78.23.197
-
-Подключение к internal:
-ssh -i ~/.ssh/appuser -A -C appuser@34.78.23.197 -t ssh 10.132.0.3
-
-devopscourses Infra repository
-bastion_IP = 34.78.23.197
-bastion_internsl_IP = 10.132.0.2
-internal_IP = 10.132.0.3
-
-pritunl
-868c7ff6250e431f86465a6453b643af
-mongodb://localhost:27017/pritunl
-
-D6:
-Деплой приложения:
-
-создание VM:
 gcloud compute instances create reddit-app\
   --project=infra-310709 \
   --zone=europe-central2-a \
@@ -32,10 +11,8 @@ gcloud compute instances create reddit-app\
   --metadata-from-file startup-script=startup_script.sh \
   --restart-on-failure
 
-создание firewall-rule:
 gcloud compute firewall-rules create "default-puma-server"\
   --allow=tcp:9292 \
   --target-tags="puma-server" \
   --source-ranges="0.0.0.0/0" \
   --description="default-puma-server-auto"
-
